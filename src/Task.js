6251a6636c7a4af73c34dabb86a951c7539ae62c
mhmadrizallll <muhammadrizal087733521@gmail.com>
2023-06-22 15:03:04 +0700

    tambah semua

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Muhammad Rizal"; // TODO: replace this
+const studentId = "FE5520798"; // TODO: replace this
+
+export { url, studentName, studentId };
